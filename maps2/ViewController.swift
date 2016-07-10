de18fcc4ec4e6d78b549a3383aebfa534f1d70bc
Chris Thompson <splashmonkey@zachs-macbook-pro.local>
2016-07-10 19:15:12 -0400

    Initial Commit

---
+++ maps2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  maps2
+//
+//  Created by Zach Jones on 7/10/16.
+//  Copyright © 2016 chris thompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
